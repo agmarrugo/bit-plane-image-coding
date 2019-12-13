c498590423a3efd4fe2f1bcb46b6bd207f76bdb6
Andres Marrugo <agmarrugo@gmail.com>
2019-12-13 11:18:54 -0500

    Initial commit

---
+++ createRGBPatternImage.m
@@ -0,0 +1,73 @@
+function rgb = createRGBPatternImage(Basename, Path, writeFile)
+% CREATERGBPATTERNIMAGE Create a 24-bit image from 1-bit pattern images
+%   This function facilitates the manual process of preparing the 
+%   24 bit depth RGB image from single 1 bit pattern images
+%   to be loaded in the DLPC350 LightCrafter.
+% 
+%   Inputs:
+%   Basename:   base filename of the patterns, e.g., bitPLane_ (assumes bmp)
+%   Path:       path to files
+%   writeFile:  Default is true so that it writes the output at out.bmp
+% 
+%   Output:
+%   rgb:        the output 24-bit depth rgb image.
+% 
+% Andres Marrugo, 2019
+
+% From the documentation:
+% 
+% The function takes all the input files and bit weights the input images 
+% according to the bit-plane position requested. Images added at bit 
+% position B0 to B7 show blue, bit position G0 to G7 show red, and 
+% bit position R0 to R7 show green. This is due to the DLPC350 display 
+% order being GRB (see table 2-69, Pattern Number Mapping, in the DLPC350 
+% Programmer's Guide), whereas BMP images are stored as RGB. For each color, 
+% bit position 0 is the least significant bit, while bit position 7 is 
+% the most significant bit.
+
+if nargin<3,
+    writeFile = true;
+end
+
+if nargin<2,
+    Path = '';
+end
+
+% LightCrafter pattern image size
+width  = 912;
+height = 1140;
+
+listFiles = dir(strcat(Path,Basename,'*'));
+
+if isempty(listFiles),
+    error('No input files were provided');
+end
+
+% listFiles(1).name
+% numel(listFiles)
+
+rgb = uint8(zeros(height,width,3));
+bit_values = uint8([1 2 4 8 16 32 64 128]);
+bit_values = [bit_values bit_values bit_values];
+
+for k = 1:numel(listFiles),
+    bitPlaneImage = uint8(im2bw(imread(strcat(Path,listFiles(k).name))));
+    
+    if k < 9,
+        rgb(:,:,2) = rgb(:,:,2) + bitPlaneImage.*bit_values(k);
+    elseif k < 17,
+        rgb(:,:,1) = rgb(:,:,1) + bitPlaneImage.*bit_values(k);
+    else
+        rgb(:,:,3) = rgb(:,:,3) + bitPlaneImage.*bit_values(k);
+    end
+    
+end
+
+if writeFile,
+    imwrite(rgb,'out.bmp');
+    fprintf('\nRGB file written to out.bmp\n');
+end
+
+
+end
+
---
+++ main.m
@@ -0,0 +1,35 @@
+%% Create 24-bit RGB from 1-bit pattern images
+% 
+% Main file for creating the 24 bit depth RGB bmp file from 1-bit pattern
+% images stored as individual bmp files
+
+
+% The 1-bit pattern images were generated and stored in a folder:
+% 
+% input_pattern_images/
+% ??? bitPlane_00.bmp
+% ??? bitPlane_01.bmp
+% ??? bitPlane_02.bmp
+% ??? bitPlane_03.bmp
+% ??? bitPlane_04.bmp
+% ??? bitPlane_05.bmp
+% ??? bitPlane_06.bmp
+% ??? bitPlane_07.bmp
+% ??? bitPlane_08.bmp
+% ??? bitPlane_09.bmp
+
+% We call the createRGBPatternImage function with the basename for the
+% images and folder path
+if isunix
+    rgb_im = createRGBPatternImage('bitPlane_', 'input_pattern_images/');
+else
+    rgb_im = createRGBPatternImage('bitPlane_', 'input_pattern_images\');
+end
+
+% The pattern images are stored in the RGB image with the following order:
+% 
+% G0, G1, ..., G7, R0, ..., R7, B0, ..., B7
+
+% The output image is stored in rgb_im and by default it is written to
+% out.bmp
+
